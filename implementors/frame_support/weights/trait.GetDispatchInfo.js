36fae4e28819c1ffafd690d545d8b8e501542de7
akru <akru@users.noreply.github.com>
2020-12-27 10:44:54 +0000

    deploy: 9ab64ebcbc024806a8514f2a3988b5b315d7365c

--- implementors/frame_support/weights/trait.GetDispatchInfo.js
+++ implementors/frame_support/weights/trait.GetDispatchInfo.js
@@ -1,7 +1,9 @@
 (function() {var implementors = {};
 implementors["ipci_runtime"] = [{"text":"impl GetDispatchInfo for Call","synthetic":false,"types":[]}];
 implementors["pallet_robonomics_datalog"] = [{"text":"impl&lt;T:&nbsp;Config&gt; GetDispatchInfo for Call&lt;T&gt;","synthetic":false,"types":[]}];
+implementors["pallet_robonomics_datalog_xcm"] = [{"text":"impl&lt;T:&nbsp;Config&gt; GetDispatchInfo for Call&lt;T&gt;","synthetic":false,"types":[]}];
 implementors["pallet_robonomics_launch"] = [{"text":"impl&lt;T:&nbsp;Config&gt; GetDispatchInfo for Call&lt;T&gt;","synthetic":false,"types":[]}];
+implementors["pallet_robonomics_launch_xcm"] = [{"text":"impl&lt;T:&nbsp;Config&gt; GetDispatchInfo for Call&lt;T&gt;","synthetic":false,"types":[]}];
 implementors["pallet_robonomics_liability"] = [{"text":"impl&lt;T:&nbsp;Config&gt; GetDispatchInfo for Call&lt;T&gt;","synthetic":false,"types":[]}];
 implementors["pallet_robonomics_rws"] = [{"text":"impl&lt;T:&nbsp;Config&gt; GetDispatchInfo for Call&lt;T&gt;","synthetic":false,"types":[]}];
 implementors["robonomics_parachain_runtime"] = [{"text":"impl GetDispatchInfo for Call","synthetic":false,"types":[]}];
