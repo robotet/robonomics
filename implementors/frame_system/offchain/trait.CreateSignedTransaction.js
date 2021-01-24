846c0f9caa1318513b239faf6387aaa0b6916e05
akru <akru@users.noreply.github.com>
2021-01-24 09:13:20 +0000

    deploy: fc0d83044ab7c59245e717d6139100645e9bbcca

--- implementors/frame_system/offchain/trait.CreateSignedTransaction.js
+++ implementors/frame_system/offchain/trait.CreateSignedTransaction.js
@@ -1,5 +1,4 @@
 (function() {var implementors = {};
 implementors["ipci_runtime"] = [{"text":"impl&lt;LocalCall&gt; CreateSignedTransaction&lt;LocalCall&gt; for Runtime <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;Call: From&lt;LocalCall&gt;,&nbsp;</span>","synthetic":false,"types":[]}];
-implementors["robonomics_parachain_runtime"] = [{"text":"impl&lt;LocalCall&gt; CreateSignedTransaction&lt;LocalCall&gt; for Runtime <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;Call: From&lt;LocalCall&gt;,&nbsp;</span>","synthetic":false,"types":[]}];
 implementors["robonomics_runtime"] = [{"text":"impl&lt;LocalCall&gt; CreateSignedTransaction&lt;LocalCall&gt; for Runtime <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;Call: From&lt;LocalCall&gt;,&nbsp;</span>","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
